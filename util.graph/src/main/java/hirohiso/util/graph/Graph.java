5b067e3e4f0a5a3ab7fb675a06a7bc0541c39458
Irie HIronori <irie.h126@gmail.com>
2019-05-02 00:38:36 +0900

    add BSDの実装

--- util.graph/src/main/java/hirohiso/util/graph/Graph.java
+++ util.graph/src/main/java/hirohiso/util/graph/Graph.java
@@ -1,24 +1,66 @@
 package hirohiso.util.graph;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Graph {
 
+    MappingSet mapping;
+
     public Graph(NodeSet nodeset, EdgeSet edgeset, MappingSet funcset) {
-        // TODO 自動生成されたコンストラクター・スタブ
+        this.mapping = funcset;
     }
 
-    public boolean hasNode(Node node1) {
-        // TODO 自動生成されたメソッド・スタブ
-        return true;
-    }
 
-    public boolean hasEdge(Node node1) {
-        // TODO 自動生成されたメソッド・スタブ
-        return true;
-    }
+    /**
+     * 現在のグラフにおいて幅優先探索を行う
+     * @param node1 開始ノード
+     * @param node2 探索ノード
+     * @return true 探索ノードを見つけた場合
+     */
+    public boolean searchBSD(Node node1, Node node2) {
+        Deque<Node> queue = new ArrayDeque<Node>();
+        Set<Node> searchedSet = new HashSet<Node>();
+        //探索経路保持用
+        HashMap<Node, Node> hashMap= new HashMap<Node, Node>();
 
-    public Object getEgde(Node node1) {
-        // TODO 自動生成されたメソッド・スタブ
-        return true;
+        //開始ノードを探索済みにする
+        searchedSet.add(node1);
+        //開始点をキューに蓄える
+        queue.push(node1);
+        Node n;//探索点
+        //キューから要素を取り出す
+        while (queue.size() != 0) {
+            n = queue.pop();
+            // 探索ノードの場合はtrueを返す
+            if (n.equals(node2)) {
+                Node now = n;
+                Node prev;
+                //探索経路を求める
+                while((prev = hashMap.get(now))!= null){
+                    System.out.println(prev);
+                    now = prev;
+                }
+                return true;
+            }
+            // 探索ノードでない場合
+            List<Node> list = this.mapping.getAdjacentNodeList(n);
+            for (Node node : list) {
+
+                if (searchedSet.contains(node)) {
+                    continue;
+                }
+                // 未探索のノードは、探索済みにしてすべてキューに追加する
+                searchedSet.add(node);
+                hashMap.put(node, n);
+                queue.push(node);
+            }
+        }
+        return false;
     }
 
 }
--- util.graph/src/test/java/hirohiso/util/graph/GraphTest.java
+++ util.graph/src/test/java/hirohiso/util/graph/GraphTest.java
@@ -84,4 +84,76 @@ public class GraphTest {
 
     }
 
+    @Test
+    public void testBSD(){
+        NodeSet nodeset = new NodeSet();
+        Node node1 = new Node();
+        Node node2 = new Node();
+        Node node3 = new Node();
+        Node node4 = new Node();
+        Node node5 = new Node();
+        Node node6 = new Node();
+        Node node99 = new Node();
+        nodeset.add(node1);
+        nodeset.add(node2);
+        nodeset.add(node3);
+        nodeset.add(node4);
+        nodeset.add(node5);
+        nodeset.add(node6);
+
+
+        EdgeSet edgeset = new EdgeSet();
+        Edge edge1 = new Edge();
+        Edge edge2 = new Edge();
+        Edge edge3 = new Edge();
+        Edge edge4 = new Edge();
+        Edge edge5 = new Edge();
+        Edge edge6 = new Edge();
+        Edge edge7 = new Edge();
+        Edge edge8 = new Edge();
+        Edge edge9 = new Edge();
+        edgeset.add(edge1);
+        edgeset.add(edge2);
+        edgeset.add(edge3);
+        edgeset.add(edge4);
+        edgeset.add(edge5);
+        edgeset.add(edge6);
+        edgeset.add(edge7);
+        edgeset.add(edge8);
+        edgeset.add(edge9);
+
+        MappingSet funcset = new MappingSet();
+        Mapping func1 = new Mapping(edge1, node1, node2);
+        Mapping func2 = new Mapping(edge2, node2, node3);
+        Mapping func3 = new Mapping(edge3, node2, node4);
+        Mapping func4 = new Mapping(edge4, node2, node5);
+        Mapping func5 = new Mapping(edge5, node1, node5);
+        Mapping func6 = new Mapping(edge6, node5, node6);
+        Mapping func7 = new Mapping(edge7, node5, node4);
+        Mapping func8 = new Mapping(edge8, node4, node3);
+        Mapping func9 = new Mapping(edge9, node4, node1);
+
+        funcset.add(func1);
+        funcset.add(func2);
+        funcset.add(func3);
+        funcset.add(func4);
+        funcset.add(func5);
+        funcset.add(func6);
+        funcset.add(func7);
+        funcset.add(func8);
+        funcset.add(func9);
+
+        Graph graph = new Graph(nodeset, edgeset, funcset);
+        //指定したノードを開始点として、次に指定したノードが存在するかを幅優先探索する
+        assertTrue(graph.searchBSD(node1,node2));
+        assertFalse(graph.searchBSD(node1,node99));
+        assertTrue(graph.searchBSD(node1,node6));
+        assertTrue(graph.searchBSD(node2,node6));
+        assertFalse(graph.searchBSD(node6,node1));
+        assertTrue(graph.searchBSD(node5,node4));
+        assertTrue(graph.searchBSD(node5,node2));
+        assertFalse(graph.searchBSD(node2,node99));
+
+    }
+
 }
