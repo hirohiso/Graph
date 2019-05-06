7e71acc2013156b5e0ca3b311cc57af26bc29726
Irie HIronori <irie.h126@gmail.com>
2019-05-06 20:48:18 +0900

    add Dfsのリファクタリング

--- util.graph/src/main/java/hirohiso/util/graph/DfsSearch.java
+++ util.graph/src/main/java/hirohiso/util/graph/DfsSearch.java
@@ -13,37 +13,37 @@ public class DfsSearch {
     public DfsSearch(MappingSet map){
         this.mapping = map;
     }
-    public boolean searchDfs(Node node1, Node node2) {
+    /**
+     * 深さ優先探索をおこなう
+     * @param 探索開始ノード
+     * @param 探索対象ノード
+     * @return
+     */
+    public boolean searchDfs(Node start, Node target) {
+        //スタック
         this.queue = new ArrayDeque<Node>();
+        //訪問済みノード格納用
         this.visitedNode = new HashSet<Node>();
 
-        this.visitedNode.add(node1);
-        this.queue.addLast(node1);
+        this.visitedNode.add(start);
+        this.queue.addLast(start);
 
         // スタックから要素nを取得する
         Node n = null;
 
         while ((n = this.queue.peekLast()) != null) {
             // 要素nがtargetの場合
-            if (n.equals(node2)) {
+            if (n.equals(target)) {
                 // 検索対象あり
                 return true;
             }
 
             // 要素nがtargetではない場合
-
             // 要素nから未訪問の隣接ノードmを取得する
-            Node m = null;
-            List<Node> list = this.mapping.getAdjacentNodeList(n);
-            for (Node i : list) {
-                if (this.visitedNode.contains(i)) {
-                    continue;
-                } else {
-                    m = i;
-                    break;
-                }
-            }
+            Node m = getUnvisitedNode(n);
+
             if (m != null) {
+                //未訪問の隣接ノードがある場合
                 // 要素mを訪問済みにし
                 this.visitedNode.add(m);
                 // 要素mをスタックに追加する
@@ -58,4 +58,19 @@ public class DfsSearch {
         // 要素nが取得できなかった場合
         return false;
     }
+
+    //未訪問の隣接ノードを一つ返却する
+    private Node getUnvisitedNode(Node n){
+        List<Node> list = this.mapping.getAdjacentNodeList(n);
+        for (Node i : list) {
+            if (this.visitedNode.contains(i)) {
+                continue;
+            } else {
+                return i;
+            }
+        }
+        //未訪問ノードがない場合はnullを返す
+        return null;
+    }
+
 }
